3b10185bc7b5f4611508efef97f3d63d0f66aed9
amanuela97 <babyzewdie@gmail.com>
2022-11-27 19:59:44 +0200

    12.9: Set up Redis for the project

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const router = express.Router();
-
+const express = require('express')
+const router = express.Router()
+const redis = require('../redis')
 const configs = require('../util/config')
 
 let visits = 0
@@ -11,8 +11,8 @@ router.get('/', async (req, res) => {
 
   res.send({
     ...configs,
-    visits
-  });
-});
+    visits,
+  })
+})
 
-module.exports = router;
+module.exports = router
